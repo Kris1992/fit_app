ed1955cb04a0a4fdcf7eb435af856a2726e994cc
Kris1992 <krzysiekrej@o2.pl>
2019-07-15 18:58:15 +0200

    Before Authentication

---
+++ src/Controller/ExerciseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExerciseController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+
+
+        return $this->render('exercise/homepage.html.twig', [
+            
+        ]);
+    }
+}
